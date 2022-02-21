b7b5a3a2851b244cbd5934c0d016e2bcc736ac69
Gokul Nair <57875699+gokuliscoding@users.noreply.github.com>
2022-02-21 15:30:01 +0530

    Add files via upload

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Search Engine</h1>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+        alt=""
+      />
+
+      <br></br>
+
+      <input type="text" classname="search-box" />
+    </div>
+  );
+};
+
+export default Home;
